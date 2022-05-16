57107acea64d4aa971cba19aa1c83ee6414fec58
sabin-aster <nepal.dev1@asterinnovations.com>
2022-05-16 13:46:35 +0545

    Latest Change

--- website/website1/views.py
+++ website/website1/views.py
@@ -26,6 +26,8 @@ def home(request):
          
         # extract the username and text field from the data
         email = request.POST['inlineFormInputGroupUsername2']
+        print(email)
+        print('-----------------')
         send_mail(
             'Email From: ' + settings.EMAIL_HOST_USER,  # subject
 
@@ -51,7 +53,7 @@ def home(request):
 #     return logo
 
 def about(request):
-    if request.method == "POST":    
+    if request.method == 'POST':    
  
         # data from the form is fetched using super function
         
@@ -77,58 +79,69 @@ def about(request):
 @csrf_exempt
 
 def contact(request):
+  
    
    
    
 
-    if  'newsletter' in request.POST:
-        email = request.POST['inlineFormInputGroupUsername2']
-        send_mail(
+   
+
+  
+    
+    
+    if  request.method=='POST': 
+        # name = request.POST['name']
+        # print(name)
+        print("ok")
+        if  'newsletter' in request.POST:
+            print('-----------')
+            email1 = request.POST['inlineFormInputGroupUsername2']
+            send_mail(
             'Email From: ' + settings.EMAIL_HOST_USER,  # subject
 
             'You are subscribed to Aster newsletter.' ,
 
 
             settings.EMAIL_HOST_USER,
-            [email, ]
-        )
-        messages.success(request, 'You\'re subscribed to aster newsletter.')
-    
-    
-    if  request.method == 'POST': 
-        # name = request.POST['name']
-        # print(name)
-        print("ok")
+            [email1, ]
+            )
+            messages.success(request, 'You\'re subscribed to aster newsletter.')
+            return render(request,'website1/home.html')
+        else:
         # ?\response_data = {}
-        name = request.POST.get('name')
+            name = request.POST.get('name')
         # email = request.POST['email']
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        phone = request.POST.get('phone')
-        response_data = {}
+            subject = request.POST.get('subject')
+            message = request.POST.get('message')
+            phone = request.POST.get('phone')
+            response_data = {}
         
         # print(email)
-        digits = "0123456789"
-        OTP = ""
-        for i in range(6) :
-            OTP += digits[math.floor(random.random() * 10)]
-        r = requests.post(
+            print(name)
+            print(message)
+            if(name != None and subject != None and message != None and phone != None):
+                print('----------------Aster-------------------')
+                digits = "0123456789"
+                OTP = ""
+                for i in range(6) :
+                    OTP += digits[math.floor(random.random() * 10)]
+                r = requests.post(
             "http://api.sparrowsms.com/v2/sms/",
             data={'token' : 'v2_Y5PTVCn5MJBm2On44pBKuQaK7of.9Gjr',
                   'from'  : 'InfoAlert',
                   'to'    : phone,
                   'text'  : 'Aster Website OTP Code: ' +OTP})
 
-        status_code = r.status_code
-        response = r.text
-        response_json = r.json()
+                status_code = r.status_code
+                response = r.text
+                response_json = r.json()
         # response_data['data'] = OTP
         
 
 
         # email=request.GET.get   ("email")
         # print(email)
-        o=OTP
+                o=OTP
         # subject, from_email, to = 'Message From Aster Website', email, settings.EMAIL_HOST_USER
         # text_content = 'This is an important message.'
         # html_content = o + ' is your one time password (OTP). Please, enter the OTP to proceed. Thank you, Aster Innovation Pvt. Ltd.'
@@ -144,24 +157,24 @@ def contact(request):
         # print(data)
         # print(data['csrfmiddlewaretoken'])
         # return redirect('/contact')
-        request.session['selected_name'] = name
+                request.session['selected_name'] = name
 
-        request.session['selected_subject'] = subject
+                request.session['selected_subject'] = subject
 
-        request.session['selected_message'] = message
+                request.session['selected_message'] = message
 
         # request.session['selected_email'] = email
-        request.session['selected_otp'] = o
-        request.session['selected_phone'] = phone
-        response_data['OTP'] = OTP
-        print(response_data)
-        print('--------------done-------------------')
+                request.session['selected_otp'] = o
+                request.session['selected_phone'] = phone
+                response_data['OTP'] = OTP
+                print(response_data)
+                print('--------------done-------------------')
         # return render(request,'website1/otp.html')
         # return redirect('otp')
-        return HttpResponse(
+                return HttpResponse(
             json.dumps(response_data),
             content_type="application/json"
-        )
+            )
    
       
 
@@ -307,54 +320,138 @@ def otp(request):
     # print(selected_name)
     # print(selected_otp)
     # print('-------------------')
+    final_selected_otp = ""
 
-    if request.method ==  'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        phone = request.POST.get('phone')
+    if request.method == 'POST':
+        # selected_name = request.session.get('selected_name')
+        # selected_subject = request.session.get('selected_subject')
+        # selected_message = request.session.get('selected_message')
+        # selected_email = request.session.get('selected_email')
+        # selected_phone = request.session.get('selected_phone')
+        # selected_otp = request.POST.get('otp')
+        # print(selected_otp)
+        if 'add_object1' in request.POST:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            # subject = request.POST.get('subject')
 
-        print(email)
-        response_data = {}
+            message  = request.POST.get('message')
+            phone = request.POST.get('phone')
+            # print(name)
+            print(email)
+            print(name)
+            print('----------------Aster-------------------')
+            digits = "0123456789"
+            OTP = ""
+            for i in range(6) :
+                OTP += digits[math.floor(random.random() * 10)]
+            final_selected_otp = OTP
+            subject, from_email, to = 'Message From Aster Website',settings.EMAIL_HOST_USER , email
+            text_content = 'This is an important message.'
+            html_content = OTP + ' is your one time password (OTP). Please, enter the OTP to proceed. Thank you, Aster Innovation Pvt. Ltd.'
+
+            request.session['selected_otp'] = OTP
+
+            selected_name = request.session['selected_name']
+            selected_subject = request.session['selected_subject']
+            selected_message = request.session['selected_message']
+            
+            request.session['selected_email'] = email
+
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
         
-        digits = "0123456789"
-        OTP = ""
-        for i in range(6) :
-            OTP += digits[math.floor(random.random() * 10)]
-        subject, from_email, to = 'Message From Aster Website',settings.EMAIL_HOST_USER , email
-        text_content = 'This is an important message.'
-        html_content = OTP + ' is your one time password (OTP). Please, enter the OTP to proceed. Thank you, Aster Innovation Pvt. Ltd.'
-       
-        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-        msg.attach_alternative(html_content, "text/html")
-        msg.send()
-        response_data['OTP'] = OTP
-        print('----------------last---------------------')
-     
-        return HttpResponse(
-            json.dumps(response_data),
-            content_type="application/json"
-        )
+            print('----------------last---------------------')
+        
 
         # send_mail('OTP request',o,settings.EMAIL_HOST_USER,[email], fail_silently=False, html_message=htmlgen)
-       
-#         if selected_otp == otp:
-#             print('##########')
-#             print(selected_name)
-#             print(selected_otp)
+        if "verify-otp" in request.POST:
+            
+            selected_otp = request.session['selected_otp']
+            selected_name = request.session['selected_name']
+            selected_subject = request.session['selected_subject']
+            selected_message = request.session['selected_message']
+            selected_email = request.session['selected_email']
+            selected_otp = selected_otp.strip()
+            otp = request.POST.get('otp')
 
-#             subject, from_email, to = 'Message From Aster Website', selected_email, settings.EMAIL_HOST_USER
-#             text_content = 'This is an important message.'
-#             # html_content = '<p> You\'ve a message. Following is the detail</p> <table border="2"> <tr> <th>Name</th> <th>Email</th> <th>Subject</th> <th>Message</th> </tr> <tr> <td>'+name+ '</td> <td> ' + email + '</td> <td> ' +subject+'</td> <td>' + message+'</td>'
-#             html_content = '''
+            print(otp)
+            print('-------------**************----------------')
+            
+                # print('##########')
+           # print(selected_name)
+                # print(selected_otp)
+                # print(selected_message)
+                # print(selected_email)
+                # print(selected_name)
+                # print(selected_subject)
+
+            def funSelectSub(subject):
+                selected_subject, from_email, to = subject, selected_email, settings.EMAIL_HOST_USER
+                text_content = 'This is an important message.'
+                html_content = '''
+            <img src="cid:logo.png" width="auto" height="150" style="padding-left:500px"/>
+            <p>You've got a message from the website. Look at below for the further details</p>
+            
+                    
+            <table  align="center" width="100%">    
+    <tr>
+      <th>Name</th> <td >'''+selected_name+'''</td>
+      <hr>
+    </tr>
+   <tr>
+     <th>Email</th> <td>'''+ selected_email+'''</td>
+     <hr>
+    </tr>
+     <tr>
+      <th>Subject</th> <td>'''+ subject+'''</td>
+    </tr>
+     <tr>
+      <th>Message</th> <td>'''+ selected_message+'''</td>
+    </tr>
+    </table>
+
+            '''
+                msg = EmailMultiAlternatives(selected_subject, text_content, from_email, [to])
+                msg.mixed_subtype = 'related'
+                msg.attach_alternative(html_content, "text/html")
+                img_dir = 'static/assets/img'
+                image = 'logo.png'
+                file_path = os.path.join(img_dir, image)
+                with open(file_path, 'rb') as f:
+                    img = MIMEImage(f.read())
+                    img.add_header('Content-ID', '<{name}>'.format(name=image))
+                    img.add_header('Content-Disposition', 'inline', filename=image)
+                    msg.attach(img)
+           
+
+           
+                # msg.content_subtype = 'html'
+                # msg.mixed_subtype = 'related'
+                # img_path = os.path.join(settings.BASE_DIR, '/static/assets/img/logo.png')
+                # with open(img_path, 'rb') as banner_image:
+                #     banner_image = MIMEImage(img_path.read())
+                #     banner_image.add_header('Content-ID', '<blogsImage.svg>')
+                #     msg.attach(banner_image)
+                    msg.send()
+                    msg.attach_alternative(html_content, "text/html")
+                    msg.send()
+
+
+
+                
+
+#                 selected_subject, from_email, to = 'Message From Aster Website', selected_email, settings.EMAIL_HOST_USER
+#                 text_content = 'This is an important message.'
+#                 html_content = '''
 #             <img src="cid:logo.png" width="auto" height="150" style="padding-left:500px"/>
 #             <p>You've got a message from the website. Look at below for the further details</p>
             
                     
 #             <table  align="center" width="100%">    
 #     <tr>
-#       <th>Name</th> <td >'''+ selected_name+'''</td>
+#       <th>Name</th> <td >'''+selected_name+'''</td>
 #       <hr>
 #     </tr>
 #    <tr>
@@ -370,46 +467,88 @@ def otp(request):
 #     </table>
 
 #             '''
-#             msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-#             msg.mixed_subtype = 'related'
-#             msg.attach_alternative(html_content, "text/html")
-#             img_dir = 'static/assets/img'
-#             image = 'logo.png'
-#             file_path = os.path.join(img_dir, image)
-#             with open(file_path, 'rb') as f:
-#                 img = MIMEImage(f.read())
-#                 img.add_header('Content-ID', '<{name}>'.format(name=image))
-#                 img.add_header('Content-Disposition', 'inline', filename=image)
-#             msg.attach(img)
+#                 msg = EmailMultiAlternatives(selected_subject, text_content, from_email, [to])
+#                 msg.mixed_subtype = 'related'
+#                 msg.attach_alternative(html_content, "text/html")
+#                 img_dir = 'static/assets/img'
+#                 image = 'logo.png'
+#                 file_path = os.path.join(img_dir, image)
+#                 with open(file_path, 'rb') as f:
+#                     img = MIMEImage(f.read())
+#                     img.add_header('Content-ID', '<{name}>'.format(name=image))
+#                     img.add_header('Content-Disposition', 'inline', filename=image)
+#                     msg.attach(img)
            
 
            
-            # msg.content_subtype = 'html'
-            # msg.mixed_subtype = 'related'
-            # img_path = os.path.join(settings.BASE_DIR, '/static/assets/img/logo.png')
-            # with open(img_path, 'rb') as banner_image:
-            #     banner_image = MIMEImage(img_path.read())
-            #     banner_image.add_header('Content-ID', '<blogsImage.svg>')
-            #     msg.attach(banner_image)
-            # msg.send()
-            # msg.attach_alternative(html_content, "text/html")
-            # msg.send()
+#                 # msg.content_subtype = 'html'
+#                 # msg.mixed_subtype = 'related'
+#                 # img_path = os.path.join(settings.BASE_DIR, '/static/assets/img/logo.png')
+#                 # with open(img_path, 'rb') as banner_image:
+#                 #     banner_image = MIMEImage(img_path.read())
+#                 #     banner_image.add_header('Content-ID', '<blogsImage.svg>')
+#                 #     msg.attach(banner_image)
+#                 msg.send()
+#                 msg.attach_alternative(html_content, "text/html")
+#                 msg.send()
+            if selected_otp == otp:
+                if selected_subject == 'I\'m interested to invest in company':
+                    funSelectSub(selected_subject)
 
-            # msg.send(fail_silently=False)
+               
 
-            # send_mail(
-            #     'From : '+ selected_email+'/ : '+ selected_subject,#subject
-            #     'Message: '+html_content,
+                elif selected_subject == 'I\'m interested to invest in a product development':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I wish to be a Regional Business/Channel Partner':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I wish to be a Province Dealer in my Country':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I wish to promote Aster Software Products to my Clients':
+                    funSelectSub(selected_subject)
 
-            #     selected_email,
-            #         ['sabinkatwalof@gmail.com'],
-            #     )
-        #     messages.success(request,'Your message has been sent sucessfully.')
-        #     return redirect('contacts')
-        # elif selected_otp != otp:
 
-        #     messages.error(request,'Your otp is not correct. Please, type the correct OTP code.')
-        #     return redirect('otp')
+                elif selected_subject == 'I am already a Aster Customer and wish to suggest':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I am already a Aster Customer and wish to complaint':
+                    funSelectSub(selected_subject)
+
+                elif selected_subject == 'I wish to have a Joint Venture (JV)':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I am a Crowd Funder amd wish to Invest in Aster':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I am a Seed Funder and wish to Invest in Aster':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I am an Angel Investor and wish to Invest in Aster':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I am looking for a Job Oppprtunity':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I am delighted to offer my enterprise Digital Business Transformation to Aster Global Team':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I want to sell my Products/Services to Aster':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I am an Ex-Employee of Aster and needs to escalate an important information':
+                    funSelectSub(selected_subject)
+                elif selected_subject == 'I have other purpose to contact the Aster Chairman':
+                    funSelectSub(selected_subject)
+                messages.success(request,'Your message has been sent sucessfully.')
+                return redirect('contacts')
+                # msg.send(fail_silently=False)
+            else:
+                messages.error(request,'Your otp is not correct. Please, type the correct OTP code.')
+                return redirect('otp')
+                # send_mail(
+                # 'From : '+ selected_email+'/ : '+ selected_subject,#subject
+                # 'Message: '+htl_content,
+
+                # selected_email,
+                #     ['sabinkatwalof@gmail.com'],
+                # )
+            
+            
+            
+            # elif final_selected_otp != otp:
+            #         messages.error(request,'Your otp is not correct. Please, type the correct OTP code.')
+            # return redirect('otp')
     return render(request,'website1/otp.html')
 
 def generateOTP() :
